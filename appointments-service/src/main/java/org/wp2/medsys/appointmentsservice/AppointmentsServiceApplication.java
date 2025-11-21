1883b0817e9a73acbb5aae12a38a772335e4fd17
domnudragota <anditu2003@gmail.com>
2025-11-21 15:36:22 +0200

    feat: add skeleton base for 3 microservices

---
+++ appointments-service/src/main/java/org/wp2/medsys/appointmentsservice/AppointmentsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.wp2.medsys.appointmentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentsServiceApplication.class, args);
+    }
+
+}
---
+++ auth-service/src/test/java/org/wp2/medsys/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wp2.medsys.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
